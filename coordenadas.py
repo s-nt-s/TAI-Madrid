17f65e28dfce7e081491afcc187224fef702f346
s-nt-s <santos82h@gmail.com>
2018-09-03 23:55:30 +0200

    coordenadas

--- coordenadas.py
+++ coordenadas.py
@@ -17,7 +17,7 @@ direcciones = yaml_from_file("data/coordenadas.yml") or {}
 nm = Nominatim(country_bias="ESP")
 
 def geocode(direccion):
-    time.sleep(1)
+    time.sleep(2)
     l = nm.geocode(direccion)
     if l:
         return l
@@ -36,26 +36,31 @@ direcciones_falta = set([(o.deDireccion, o.dire, o.postCode) for o in organismos
 total = len(direcciones_falta)
 count = 0
 ok = 0
+_ok = 0
 print ("Calculando coordenadas (%s)" % total)
 for d1, d2, p in direcciones_falta:
     if d2 in direcciones:
         continue
+    pl1, pl2, resto = d1.split(" ", 2)
+    if pl1.lower() in ("avda", "avda.", "av."):
+        pl1 = "Avenida"
+    d1 = " ".join([pl1, pl2, resto])
     l = geocode(d1)
     if l is None and d1.count(" ")>1:
-        pl1, pl2, resto = d1.split(" ", 2)
         if pl2.lower() not in ("de", "del"):
             aux = pl1 +" de "+ pl2+" "+resto
-            l = geocode(d1)
+            l = geocode(aux)
             if l is None:
-                aux = pl1 +" de "+ pl2+" "+resto
-                l = geocode(d1)
+                aux = pl1 +" del "+ pl2+" "+resto
+                l = geocode(aux)
     if l and p in l.address:
         direcciones[d2] = str(l.latitude)+","+str(l.longitude)
         ok += 1
     count += 1
     print("%3d%% completado: %-30s (%s)" % (count * 100 / total, d1[:30], ok), end="\r")
-    if ok % 10 == 0:
+    if ok % 10 == 0 and ok>_ok:
         yaml_to_file("data/coordenadas.yml", direcciones)
+        _ok=ok
 
 print ("")
 yaml_to_file("data/coordenadas.yml", direcciones)
