7d0bacc2c0f7f2ddaacc6ac14680c43563be0268
s-nt-s <santos82h@gmail.com>
2018-09-10 19:24:03 +0200

    coordenadas

--- fuentes/get-links.py
+++ fuentes/get-links.py
@@ -5,6 +5,10 @@ from urllib.parse import urljoin
 
 import bs4
 import requests
+import re
+from datetime import datetime
+
+re_sp = re.compile(r"\s+")
 
 root = "http://transparencia.gob.es/transparencia/transparencia_Home/index/PublicidadActiva/OrganizacionYEmpleo/Relaciones-Puestos-Trabajo.html"
 
@@ -34,12 +38,21 @@ def get(url):
     return soup
 
 
-def get_pdf_boe(boe, descripcion):
+def get_pdf_boe(boe, descripcion=None):
     print ('')
-    print ('# %s - %s' % (boe, descripcion))
     soup = get("http://www.boe.es/diario_boe/txt.php?id=" + boe)
+    if descripcion is None:
+        descripcion = re_sp.sub(" ", soup.find("h3").get_text()).strip()
+    print ('# %s - %s' % (boe, descripcion))
     url = soup.select("li.puntoPDF a")[0].attrs["href"]
     print(url)
+
+def get_nombramientos():
+    desde = "31/03/2018".replace("/", "%2F")
+    hasta = datetime.today().strftime("%Y/%m/%d").replace("/", "%2F")
+    url = "https://www.boe.es/buscar/personal.php?campo%5B0%5D=TIT&dato%5B0%5D=&operador%5B0%5D=and&campo%5B1%5D=ID_RNG&dato%5B1%5D=&operador%5B1%5D=and&campo%5B2%5D=ID_DEM&dato%5B2%5D=&operador%5B2%5D=and&campo%5B3%5D=DOC&dato%5B3%5D=Nombramientos%2C+situaciones+e+incidencias&operador%5B3%5D=and&campo%5B4%5D=NBO&dato%5B4%5D=&operador%5B4%5D=and&campo%5B5%5D=DOC&dato%5B5%5D=&operador%5B6%5D=and&campo%5B6%5D=FPU&dato%5B6%5D%5B0%5D=" + desde + "&dato%5B6%5D%5B1%5D=" + hasta + "&operador%5B7%5D=and&campo%5B7%5D=FAP&dato%5B7%5D%5B0%5D=&dato%5B7%5D%5B1%5D=&page_hits=2000&sort_field%5B0%5D=FPU&sort_order%5B0%5D=desc&sort_field%5B1%5D=ref&sort_order%5B1%5D=asc&accion=Buscar"
+    soup = get(url)
+    
 
 soup = get(root)
 
@@ -61,6 +74,13 @@ get_pdf_boe('BOE-A-2016-12467', '2015 Libre Nombramientos')
 
 # get_pdf_boe('BOE-A-2016-7615', '2015 Interna Opositories')
 
+'''
+print ('# Puestos que ya no son vacantes')
+get_pdf_boe('BOE-A-2018-10329')
+get_pdf_boe('BOE-A-2018-10302')
+get_pdf_boe('BOE-A-2018-9733')
+get_pdf_boe('BOE-A-2018-9564')
+'''
 
 visto = set()
 for i in soup.select("section#block_content_ministerios a"):
